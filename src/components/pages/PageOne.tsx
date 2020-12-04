68692ff6e51af354d63e2e09ad2601f3854e4a41
bbawuah <brianbawuah@icloud.com>
2020-12-04 13:57:03 +0100

    .

--- src/components/pages/PageOne.tsx
+++ src/components/pages/PageOne.tsx
@@ -5,7 +5,7 @@ const PageOne = () => (
     <div className="home-component">
         <Layout>
          <div className="text-container">
-         <h1>Veiligheid in Zuid-oost</h1>
+         <h1>Crimninaliteit in Zuid-oost</h1>
             <p>In het nieuws wordt er veel aandacht besteed aan criminaliteit. Nieuws over messen, fraude of kinderporno bijvoorbeeld. Het beeld dat van criminaliteit geschetst wordt is ernstig, maar hebben we wel een goed beeld van welk type criminaliteit het meeste voorkomt? Zijn we wel goed geïnformeerd over de aarde van criminaliteit?</p>
             <p>Daarom hebben wij een onderzoek gestart waarbij we de criminaliteitscijfers van de afgelopen twee maanden  in Amsterdam Zuid-Oost bekijken. We bekijken:</p>
             <ul>
@@ -14,7 +14,7 @@ const PageOne = () => (
             </ul>
 
             <p>
-            In onze infographic kun je deze met elkaar vergelijken en zo tot je eigen inzicht komen over de veiligheid van Amsterdam-Zuidoost!
+            In onze infographic kun je deze met elkaar vergelijken en zo tot je eigen inzicht komen over de Crimninaliteit van Amsterdam-Zuidoost!
             </p>
             <p>Elias Poolman en Brian Bawuah</p>
             <p>Dataweek CMD 2020</p>
